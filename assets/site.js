5fe08a508d881e35e1b102d34e9ac4905aec6ea2
Muhammad Farzad Ali <farzad0514@gmail.com>
2026-06-23 17:39:41 +0200

    update files 3

--- assets/site.js
+++ assets/site.js
@@ -20,25 +20,3 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
-document.addEventListener("DOMContentLoaded", () => {
-  const currentYear = document.querySelectorAll("[data-current-year]");
-  currentYear.forEach((node) => {
-    node.textContent = new Date().getFullYear();
-  });
-
-  const menuButton = document.querySelector("[data-menu-button]");
-  const mobileMenu = document.querySelector("[data-mobile-menu]");
-
-  if (menuButton && mobileMenu) {
-    menuButton.addEventListener("click", () => {
-      const isHidden = mobileMenu.hasAttribute("hidden");
-      if (isHidden) {
-        mobileMenu.removeAttribute("hidden");
-        menuButton.setAttribute("aria-expanded", "true");
-      } else {
-        mobileMenu.setAttribute("hidden", "");
-        menuButton.setAttribute("aria-expanded", "false");
-      }
-    });
-  }
-});
